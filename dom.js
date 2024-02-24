976e82b31bf8da13cbc19681ec001de8625d3d81
DHRUV-GUPTA51 <dhruvgupta2651@gmail.com>
2024-02-24 18:23:02 +0530

    ere

--- dom.js
+++ dom.js
@@ -40,6 +40,42 @@ divi.style.backgroundColor = "orange"
 
 const alter = document.createTextNode('HARII BOLL')
 divi.appendChild(alter)
+
+//below line tells us how to attach the javascript code through the body and hence returning the output on the screen
+
 document.body.appendChild(divi);
 
 // document.body.innerText(divi)
+
+
+// everything is different
+function addLanguage(langName) {
+    const li = document.createElement('li');
+    li.innerHTML = `${langName}`
+    document.querySelector('.language').appendChild(li)
+}
+addLanguage("python")
+addLanguage("typescript")
+
+function addOptiLanguage(langName) {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(langName))
+    document.querySelector('.language').appendChild(li)
+}
+addOptiLanguage('golang')
+
+//Edit
+const secondLang = document.querySelector("li:nth-child(2)")
+console.log(secondLang);
+//secondLang.innerHTML = "Mojo"
+const newli = document.createElement('li')
+newli.textContent = "Mojo"
+secondLang.replaceWith(newli)
+
+//edit
+const firstLang = document.querySelector("li:first-child")
+firstLang.outerHTML = '<li>TypeScript</li>'
+
+//remove
+const lastLang = document.querySelector('li:last-child')
+lastLang.remove()
---
+++ project_with_me/project1.md
@@ -0,0 +1,15 @@
+# project related to DOM
+
+## project file link 
+
+## https://stackblitz.com/edit/dom-project-chaiaurcode?file=index.html
+
+# solutioin code 
+## project 1
+
+```javascript
+console.log("dhruv")
+
+```
+
+
